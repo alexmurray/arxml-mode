bec1f2e6c7523b8cbba47ad07541a4a8b8d504e1
Alex Murray <murray.alex@gmail.com>
2017-06-21 15:54:20 +0930

    Add flycheck support using offical xsd schema

--- arxml-mode.el
+++ arxml-mode.el
@@ -2,16 +2,45 @@
 
 ;;; Commentary:
 
+;; TODO: Add yasnippet integration like xproc-mode
+;; https://github.com/bertfrees/xproc-mode for common snippets
 ;;; Code:
 (require 'nxml-mode)
+;; try load flycheck if available
+(require 'flycheck nil t)
 
 (define-derived-mode arxml-mode nxml-mode "arxml"
-  "Major mode for editing arxml files.")
+  "Major mode for editing arxml files."
+  (when (boundp 'flycheck-checkers)
+    (add-to-list 'flycheck-checkers 'arxml-xmllint)))
 
 ;; use local schemas.xml to find our local AUTOSAR_00042.rnc
-(add-to-list 'rng-schema-locating-files (concat (file-name-directory load-file-name)
+(add-to-list 'rng-schema-locating-files (concat (file-name-directory
+                                                 (or load-file-name buffer-file-name))
                                                 "schemas.xml"))
 (add-to-list 'auto-mode-alist '("\\.arxml\\'" . arxml-mode))
+
+(when (fboundp 'flycheck-define-checker)
+  (flycheck-def-option-var flycheck-arxml-schema-path
+      (concat (file-name-directory
+               (or load-file-name buffer-file-name))
+              "AUTOSAR_00042.xsd")
+      arxml-xmllint
+    "Path to the schema to validate ARXML files against.")
+
+  (flycheck-define-checker arxml-xmllint
+    "A ARXML syntax checker and validator using the xmllint utility.
+
+The xmllint is part of libxml2, see URL
+`http://www.xmlsoft.org/'."
+    :command ("xmllint" "--noout"
+              (option "--schema" flycheck-arxml-schema-path nil)
+              "-")
+
+    :standard-input t
+    :error-patterns
+    ((error line-start "-:" line ": " (message) line-end))
+    :modes (arxml-mode)))
 
 (provide 'arxml-mode)
 
