6161182d3b08d8ddccf53ee0de2332725e8ed6da
Alex Murray <murray.alex@gmail.com>
2017-06-23 22:28:48 +0930

    Require all packages and fix xref implementation
    Use an alist based on symbol name to store - easier than a hash table

--- arxml-mode.el
+++ arxml-mode.el
@@ -14,123 +14,118 @@
 (require 'cl-lib)
 (require 'subr-x)
 (require 'xref)
+(require 'yasnippet)
+(require 'flycheck)
 
-;; try and dynamically setup yasnippet snippets if available
-(when (and (require 'yasnippet nil t)
-           (boundp 'yas/snippet-dirs))
-  (push (expand-file-name "snippets" (file-name-directory
-                                      (or load-file-name buffer-file-name)))
-        yas/snippet-dirs))
+;; yasnippet integration
+(push (expand-file-name "snippets" (file-name-directory
+                                    (file-truename
+                                     (expand-file-name
+                                      (or load-file-name buffer-file-name)))))
+      yas/snippet-dirs)
 
-;; try and dynamically integrate with flychck
-(when (and (require 'flycheck nil t)
-           (fboundp 'flycheck-define-checker)
-           (fboundp 'flycheck-def-option-var))
-  (flycheck-def-option-var flycheck-arxml-schema-path
-      (concat (file-name-directory
-               (or load-file-name buffer-file-name))
-              "AUTOSAR_00042.xsd")
-      arxml-xmllint
-    "Path to the schema to validate ARXML files against.")
+;; flycheck integration
+(flycheck-def-option-var flycheck-arxml-schema-path
+    (concat (file-name-directory
+             (file-truename
+              (expand-file-name
+               (or load-file-name buffer-file-name))))
+            "AUTOSAR_00042.xsd")
+    arxml-xmllint
+  "Path to the schema to validate ARXML files against.")
 
-  (flycheck-define-checker arxml-xmllint
-    "A ARXML syntax checker and validator using the xmllint utility.
+(flycheck-define-checker arxml-xmllint
+  "A ARXML syntax checker and validator using the xmllint utility.
 
 The xmllint is part of libxml2, see URL
 `http://www.xmlsoft.org/'."
-    :command ("xmllint" "--noout"
-              (option "--schema" flycheck-arxml-schema-path nil)
-              "-")
-    :standard-input t
-    :error-patterns
-    ((error line-start "-:" line ": " (message) line-end))
-    :modes (arxml-mode)))
+  :command ("xmllint" "--noout"
+            (option "--schema" flycheck-arxml-schema-path nil)
+            "-")
+  :standard-input t
+  :error-patterns
+  ((error line-start "-:" line ": " (message) line-end))
+  :modes (arxml-mode))
 
-;; hash table to store index data
-(define-hash-table-test 'string-equal-test 'string= 'sxhash)
-
+;; table to store index data
 (defvar arxml-tags-table nil)
 
 (defun arxml-parse-index (&optional index)
   "Parse the index specified by INDEX."
   (interactive "fIndex file:")
   (with-current-buffer (find-file-noselect index)
-    (if arxml-tags-table
-        (clrhash arxml-tags-table)
-      (setq arxml-tags-table (make-hash-table :test #'string-equal-test)))
+    (setq arxml-tags-table nil)
     (goto-char (point-min))
     (while (re-search-forward
             "^\\([dr]\\) \\([^ ]+\\) \\([^ ]+\\) \\([0-9]+\\) \\([0-9]+\\)$"
-            (point-max) t)
+            nil t)
       (let ((type (substring-no-properties (match-string 1)))
-            (name (substring-no-properties (match-string 2)))
+            (symbol (intern (substring-no-properties (match-string 2))))
             (file (substring-no-properties (match-string 3)))
             (line (string-to-number (substring-no-properties (match-string 4))))
             (col  (string-to-number (substring-no-properties (match-string 5)))))
         ;; see if already in table and update
-        (let ((entry (gethash name arxml-tags-table)))
-          (unless entry
-            ;; create a new entry
-            (setq entry '((def . nil)
-                          (ref . nil)))
-            (puthash name entry arxml-tags-table))
+        (let ((tag (alist-get symbol arxml-tags-table)))
+          (unless tag
+            (setq tag `((symbol . ,symbol)
+                        (def . ,nil)
+                        (ref . ,nil)))
+            (message "New tag %S" tag))
+          (setf (alist-get symbol arxml-tags-table) tag)
+          (message "before: %S" (alist-get symbol arxml-tags-table))
+          (message "%s %S %s %d %d" type symbol file line col)
           (pcase type
-            ("d" (setf (cdr (assoc 'def entry)) `((file . ,file)
-                                                  (line . ,line)
-                                                  (col . ,col))))
-            ("r" (setf (cdr (assoc 'ref entry))
-                       (push `((file . ,file)
-                               (line . ,line)
-                               (col . ,col))
-                             (cdr (assoc 'ref entry)))))
-            (_ (error "Unknown type %S" type))))))))
+            ("d" (push `((file . ,file)
+                         (line . ,line)
+                         (col . ,col))
+                       (alist-get 'def tag)))
+            ("r" (push `((file . ,file)
+                         (line . ,line)
+                         (col . ,col))
+                       (alist-get 'ref tag)))
+            (_ (error "Unknown type %S" type)))
+          (message "after: %S" (alist-get symbol arxml-tags-table)))))))
 
-(defun arxml-find-entry (type symbol)
+(defun arxml-find-tag (type symbol)
   "Return an alist of the TYPE of SYMBOL from the index file."
   (unless arxml-tags-table
-    (arxml-parse-index (read-file-name "Index file:" nil "index")))
-  (let ((entry (gethash symbol arxml-tags-table)))
-    (cdr (assoc type entry))))
-
-(defun arxml-find-definition (symbol)
-  "Return an alist of the definition of SYMBOL from the index file."
-  (arxml-find-entry 'def symbol))
-
-(defun arxml-find-references (symbol)
-  "Return a list of alists of the references of SYMBOL from the index file."
-  (arxml-find-entry 'ref symbol))
+    (arxml-parse-index (if (file-exists-p "index")
+                           "index"
+                         (read-file-name "Index file:" nil "index"))))
+  (let ((tag (alist-get symbol arxml-tags-table)))
+    (assert (or (null tag)
+                (eq (alist-get 'symbol tag) symbol)))
+    (message "%s - %S" symbol tag)
+    (alist-get type tag)))
 
 ;; only active when in arxml-mode
 (defun arxml-xref-backend ()
-  "arxml backend for Xref."
+  "Xref backend for arxml."
   (when (eq major-mode 'arxml-mode)
     'arxml))
+
+(defun arxml-get-xrefs (type symbol)
+  "Return a list of xref locations of TYPE for SYMBOL."
+  (remove nil
+          (mapcar #'(lambda (tag)
+                      (if tag
+                          (xref-make (symbol-name symbol)
+                                     (xref-make-file-location (alist-get 'file tag)
+                                                              (alist-get 'line tag)
+                                                              (alist-get 'col tag)))))
+                  (arxml-find-tag type symbol))))
 
 (cl-defmethod xref-backend-identifier-at-point ((_backend (eql arxml)))
   (let* ((current (substring-no-properties (symbol-name (symbol-at-point))))
          (matched (string-match ".*>\\(.*\\)<.*" current))
          (identifier (if matched (match-string 1 current))))
-    identifier))
+    (intern identifier)))
 
 (cl-defmethod xref-backend-definitions ((_backend (eql arxml)) symbol)
-  (remove nil
-          (mapcar #'(lambda (def)
-                      (if def
-                          (xref-make symbol
-                                     (xref-make-file-location (cdr (assoc 'file def))
-                                                              (cdr (assoc 'line def))
-                                                              (cdr (assoc 'col def))))))
-                  (list (arxml-find-definition symbol)))))
+  (arxml-get-xrefs 'def symbol))
 
 (cl-defmethod xref-backend-references ((_backend (eql arxml)) symbol)
-  (remove nil
-          (mapcar #'(lambda (ref)
-                      (if ref
-                          (xref-make symbol
-                                     (xref-make-file-location (cdr (assoc 'file ref))
-                                                              (cdr (assoc 'line ref))
-                                                              (cdr (assoc 'col ref))))))
-                  (arxml-find-references symbol))))
+  (arxml-get-xrefs 'ref symbol))
 
 (cl-defmethod xref-backend-identifier-completion-table ((_backend (eql arxml)))
   (hash-table-keys arxml-tags-table))
