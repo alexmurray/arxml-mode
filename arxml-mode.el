b5d05c11847baf05bcd113cf413bdf3dda368f71
Alex Murray <murray.alex@gmail.com>
2017-06-29 11:44:55 +0930

    Cleanups - use cl-struct and hash table plus naming etc

--- arxml-mode.el
+++ arxml-mode.el
@@ -18,6 +18,12 @@
 (require 'yasnippet)
 (require 'flycheck)
 
+;; path to the AUTOSAR_00042.xsd shipped with arxml-mode.el
+(defvar arxml-mode-xsd-path (concat (file-name-directory
+                                (file-truename
+                                 (expand-file-name
+                                  (or load-file-name buffer-file-name))))
+                               "AUTOSAR_00042.xsd"))
 ;; yasnippet integration
 (push (expand-file-name "snippets" (file-name-directory
                                     (file-truename
@@ -26,94 +32,79 @@
       yas/snippet-dirs)
 
 ;; flycheck integration
-(flycheck-def-option-var flycheck-arxml-schema-path
-    (concat (file-name-directory
-             (file-truename
-              (expand-file-name
-               (or load-file-name buffer-file-name))))
-            "AUTOSAR_00042.xsd")
-    arxml-xmllint
-  "Path to the schema to validate ARXML files against.")
+(flycheck-add-mode 'xml-xmllint 'arxml-mode)
+(flycheck-add-mode 'xml-xmlstarlet 'arxml-mode)
 
-(flycheck-define-checker arxml-xmllint
-  "A ARXML syntax checker and validator using the xmllint utility.
-
-The xmllint is part of libxml2, see URL
-`http://www.xmlsoft.org/'."
-  :command ("xmllint" "--noout"
-            (option "--schema" flycheck-arxml-schema-path nil)
-            "-")
-  :standard-input t
-  :error-patterns
-  ((error line-start "-:" line ": " (message) line-end))
-  :modes (arxml-mode))
-
+;; xref integration
 ;; table to store index data
-(defvar arxml-tags-table nil)
+(defvar arxml-mode-tags-table nil)
 
-(defvar arxml-tags-list nil)
+;; list to store tag names for xref-apropos and completion-at-point
+(defvar arxml-mode-tags-list nil)
 
-(defun arxml-parse-index (&optional index)
+(cl-defstruct arxml-mode-tag name def ref)
+
+(cl-defstruct arxml-mode-tag-location file line col)
+
+(defun arxml-mode-parse-index (&optional index)
   "Parse the index specified by INDEX."
   (interactive "fIndex file:")
   (with-current-buffer (find-file-noselect index)
-    (setq arxml-tags-table nil)
-    (setq arxml-tags-list nil)
+    (setq arxml-mode-tags-table (make-hash-table :test 'equal))
+    (setq arxml-mode-tags-list nil)
     (goto-char (point-min))
     (while (re-search-forward
             "^\\([dr]\\) \\([^ ]+\\) \\([^ ]+\\) \\([0-9]+\\) \\([0-9]+\\)$"
             nil t)
       (let ((type (substring-no-properties (match-string 1)))
-            (symbol (intern (substring-no-properties (match-string 2))))
+            (name (substring-no-properties (match-string 2)))
             (file (substring-no-properties (match-string 3)))
             (line (string-to-number (substring-no-properties (match-string 4))))
             (col  (string-to-number (substring-no-properties (match-string 5)))))
         ;; see if already in table and update
-        (let ((tag (alist-get symbol arxml-tags-table)))
+        (let ((tag (gethash name arxml-mode-tags-table)))
           (unless tag
-            (setq tag `((symbol . ,symbol)
-                        (def . ,nil)
-                        (ref . ,nil)))
-            (push (symbol-name symbol) arxml-tags-list)
-            (setf (alist-get symbol arxml-tags-table) tag))
-          (pcase type
-            ("d" (push `((file . ,file)
-                         (line . ,line)
-                         (col . ,col))
-                       (alist-get 'def tag)))
-            ("r" (push `((file . ,file)
-                         (line . ,line)
-                         (col . ,col))
-                       (alist-get 'ref tag)))
-            (_ (error "Unknown type %S" type))))))))
+            (setq tag (make-arxml-mode-tag :name name :def nil :ref nil))
+            (push name arxml-mode-tags-list)
+            (puthash name tag arxml-mode-tags-table))
+          (let ((location (make-arxml-mode-tag-location :file file :line line :col col)))
+            (pcase type
+              ("d" (push location (arxml-mode-tag-def tag)))
+              ("r" (push location (arxml-mode-tag-ref tag)))
+              (_ (error "Unknown type %S" type)))))))))
 
-(defun arxml-find-tag (type tag)
-  "Return an alist of the TYPE of TAG from the index file."
-  (unless arxml-tags-table
-    (arxml-parse-index (if (file-exists-p "index")
+(defun arxml-mode-find-tag-location (type name)
+  "Return an arxml-mode-tag-location of the TYPE of NAME from the index file."
+  (unless arxml-mode-tags-table
+    (arxml-mode-parse-index (if (file-exists-p "index")
                            "index"
                          (read-file-name "Index file:" nil "index"))))
-  (let ((tag (alist-get (intern tag) arxml-tags-table)))
-    (alist-get type tag)))
+  (let ((tag (gethash name arxml-mode-tags-table)))
+    (when tag
+      (pcase type
+        ('def (arxml-mode-tag-def tag))
+        ('ref (arxml-mode-tag-ref tag))
+        (_ (error "Unknown type %S" type))))))
 
 ;; only active when in arxml-mode
-(defun arxml-xref-backend ()
+(defun arxml-mode-xref-backend ()
   "Xref backend for arxml."
   (when (eq major-mode 'arxml-mode)
     'arxml))
 
-(defun arxml-get-xrefs (type name)
+(defun arxml-mode-get-xrefs (type name)
   "Return a list of xref locations of TYPE for NAME."
   (remove nil
           (mapcar #'(lambda (tag)
                       (if tag
                           (xref-make name
-                                     (xref-make-file-location (alist-get 'file tag)
-                                                              (alist-get 'line tag)
-                                                              (alist-get 'col tag)))))
-                  (arxml-find-tag type name))))
+                                     (xref-make-file-location
+                                      (arxml-mode-tag-location-file tag)
+                                      (arxml-mode-tag-location-line tag)
+                                      (arxml-mode-tag-location-col tag)))))
+                  (arxml-mode-find-tag-location type name))))
 
-(defun arxml-identifier-at-point ()
+(defun arxml-mode-identifier-at-point ()
   "Get the arxml identifier at point."
   (let* ((current (thing-at-point 'symbol t))
          (start (car (bounds-of-thing-at-point 'symbol)))
@@ -127,54 +118,62 @@ The xmllint is part of libxml2, see URL
         (end . ,end)))))
 
 (cl-defmethod xref-backend-identifier-at-point ((_backend (eql arxml)))
-  (alist-get 'identifier (arxml-identifier-at-point)))
+  (alist-get 'identifier (arxml-mode-identifier-at-point)))
 
 (cl-defmethod xref-backend-definitions ((_backend (eql arxml)) identifier)
-  (arxml-get-xrefs 'def identifier))
+  (arxml-mode-get-xrefs 'def identifier))
 
 (cl-defmethod xref-backend-references ((_backend (eql arxml)) identifier)
-  (arxml-get-xrefs 'ref identifier))
+  (arxml-mode-get-xrefs 'ref identifier))
 
 (cl-defmethod xref-backend-apropos ((_backend (eql arxml)) identifier)
+  (unless arxml-mode-tags-list
+    (arxml-mode-parse-index (if (file-exists-p "index")
+                                "index"
+                              (read-file-name "Index file:" nil "index"))))
   (let ((tags))
-    (dolist (tag arxml-tags-table)
-      (let ((name (symbol-name (car tag))))
+    (dolist (name arxml-mode-tags-list)
+      (let ((tag (gethash name arxml-mode-tags-table)))
         (when (string-match identifier name)
-          (dolist (tag (alist-get 'def (cdr tag)))
+          (dolist (location (arxml-mode-tag-def tag))
             (push (xref-make name
-                             (xref-make-file-location (alist-get 'file tag)
-                                                      (alist-get 'line tag)
-                                                      (alist-get 'col tag)))
+                             (xref-make-file-location
+                              (arxml-mode-tag-location-file location)
+                              (arxml-mode-tag-location-line location)
+                              (arxml-mode-tag-location-col location)))
                   tags)))))
     tags))
 
 (cl-defmethod xref-backend-identifier-completion-table ((_backend (eql arxml)))
-  (unless arxml-tags-list
-    (arxml-parse-index (if (file-exists-p "index")
-                           "index"
-                         (read-file-name "Index file:" nil "index"))))
-  arxml-tags-list)
+  (unless arxml-mode-tags-list
+    (arxml-mode-parse-index (if (file-exists-p "index")
+                                "index"
+                              (read-file-name "Index file:" nil "index"))))
+  arxml-mode-tags-list)
 
 ;; completion at point
-(defun arxml-completion-at-point ()
-  (let ((identifier (arxml-identifier-at-point)))
+(defun arxml-mode-completion-at-point ()
+  "`completion-at-point' function for arxml-mode."
+  (let ((identifier (arxml-mode-identifier-at-point)))
     (when identifier
-      (unless arxml-tags-table
-        (arxml-parse-index (if (file-exists-p "index")
-                               "index"
-                             (read-file-name "Index file:" nil "index"))))
+      (unless arxml-mode-tags-table
+        (arxml-mode-parse-index (if (file-exists-p "index")
+                                    "index"
+                                  (read-file-name "Index file:" nil "index"))))
       (list (alist-get 'begin identifier)
             (alist-get 'end identifier)
-            arxml-tags-list
+            arxml-mode-tags-list
             :exclusive 'no
             :company-docsig #'identity))))
 
 ;; define our major-mode
 (define-derived-mode arxml-mode nxml-mode "arxml"
   "Major mode for editing arxml files."
-  (add-to-list 'flycheck-checkers 'arxml-xmllint)
-  (add-to-list 'xref-backend-functions 'arxml-xref-backend)
-  (add-to-list 'completion-at-point-functions #'arxml-completion-at-point))
+  (add-to-list 'xref-backend-functions 'arxml-mode-xref-backend)
+  (add-to-list 'completion-at-point-functions #'arxml-mode-completion-at-point)
+  ;; integrate with flycheck
+  (setq flycheck-xml-xmlstarlet-xsd-path arxml-mode-xsd-path)
+  (setq flycheck-xml-xmllint-xsd-path flycheck-xml-xmlstarlet-xsd-path))
 
 ;; use local schemas.xml to find our local AUTOSAR_00042.rnc
 (add-to-list 'rng-schema-locating-files (concat (file-name-directory
